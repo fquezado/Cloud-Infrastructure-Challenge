5c08151f307fe25de90f951204381453fb4bc23c
fquezado <fquezado18@gmail.com>
2026-09-01 19:21:11 -0400

    adding subnet to ec2 instance

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  subnet_id     = aws_subnet.private_subnets[*].id
 
   tags = {
     Name = "EC2Instance"
