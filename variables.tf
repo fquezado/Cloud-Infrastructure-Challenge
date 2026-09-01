9fe2231ed220a9ec06f56a3bf975753cc067b89f
fquezado <fquezado18@gmail.com>
2026-09-01 17:33:56 -0400

    fixing variables to add for public and private subnets

--- variables.tf
+++ variables.tf
@@ -25,13 +25,23 @@ variable "key_name" {
   type        = string
 }
 
-variable "vpc_id" {
-  description = "The VPC ID where the security groups will be created"
-  type        = string
-}
-
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
   default     = null
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones (I am staying on east coast)"
+  type        = list(string)
 }
