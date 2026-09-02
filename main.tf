bc69da2302d85c8f0b63d13fcfabebf6bd405035
fquezado <fquezado18@gmail.com>
2026-09-01 23:49:05 -0400

    changing instance type in variables and also changing to get most recent ami

--- main.tf
+++ main.tf
@@ -1,5 +1,25 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-*-26.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 resource "aws_instance" "ec2_instance" {
-  ami                    = var.ami_id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = var.key_name
   subnet_id              = aws_subnet.private_subnet[0].id // Using one of two private subnets created.
--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "The instance type for the EC2 instance"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
