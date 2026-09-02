ee019b9c68818a765b1c8857c57d775e57fa3ea1
fquezado <fquezado18@gmail.com>
2026-09-02 00:23:10 -0400

    making script be loaded and created with the ec2 instance

--- main.tf
+++ main.tf
@@ -19,11 +19,23 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
   //key_name               = var.key_name
   subnet_id              = aws_subnet.private_subnet[0].id // Using one of two private subnets created.
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data                   = file("${path.module}/scripts/web_server_install.sh")
+  user_data_replace_on_change = true
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = "EC2Instance"
