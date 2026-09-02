be6e94c7bac3633ac0b8f30f82cbf010b69f0a36
fquezado <fquezado18@gmail.com>
2026-09-02 00:09:00 -0400

    adding git ignore and fixing ami and getting rid of key pair - not needed at this moment

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-*-26.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-resolute-26.04-amd64-server-*"]
   }
 
   filter {
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  key_name               = var.key_name
+  //key_name               = var.key_name
   subnet_id              = aws_subnet.private_subnet[0].id // Using one of two private subnets created.
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
