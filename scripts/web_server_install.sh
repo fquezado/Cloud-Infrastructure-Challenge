2803928126f69fcbd9520181b76ce4d952358bfd
fquezado <fquezado18@gmail.com>
2026-09-02 00:29:16 -0400

    fixing bash script

--- scripts/web_server_install.sh
+++ scripts/web_server_install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
 apt-get install -y nginx
-echo "<h1>This is Fernando's Cloud Infrastructure ENG take home exercise - GO RAVENS</h1>" >
-/var/www/html/index.html
+
+echo "<h1>This is Fernando's Cloud Infrastructure ENG take home exercise</h1>\
+<p>GO RAVENS</p>" > /var/www/html/index.html
+
 systemctl enable --now nginx
