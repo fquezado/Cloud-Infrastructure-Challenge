945ebc2c62c93275df3e5561b326b4b3dd1aa321
fquezado <fquezado18@gmail.com>
2026-09-02 02:29:02 -0400

    adding Certificate manager provider code

--- versions.tf
+++ versions.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    // Certificate Manager Provider
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
