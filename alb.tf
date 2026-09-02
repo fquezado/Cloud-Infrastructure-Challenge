aff0fe5b6b1467bb6c13a4dffb367d3d64fde5fa
fquezado <fquezado18@gmail.com>
2026-09-02 02:16:41 -0400

    adding target group attachment and also fixing listener to redirect

--- alb.tf
+++ alb.tf
@@ -31,13 +31,24 @@ resource "aws_lb_target_group" "alb_target_group" {
   }
 }
 
-resource "aws_lb_listener" "alb_listener" {
+resource "aws_lb_target_group_attachment" "alb_target_group_attachment" {
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id        = aws_instance.ec2_instance.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "http_redirect" {
   load_balancer_arn = aws_lb.alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_target_group.arn
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
